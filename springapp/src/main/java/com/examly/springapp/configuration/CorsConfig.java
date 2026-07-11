0fe717eb94b6fd556f0e86714320689312ac49c2
secondary-test <peakmanpeak@gmail.com>
2026-07-11 12:32:35 +0530

    closes #4 , closes #5 , closes #6 , closes #7 , closes #8 , closes #9

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfig.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
@@ -18,5 +18,6 @@ public class CorsConfig implements WebMvcConfigurer {
 	}
 }
 public class CorsConfig {
+	
     
 }
